5d4e8629fd6b1f4fad41c5a7a0ffb909aec22148
Ridho Kurniawan <ridhokurniawan633@gmail.com>
2023-10-24 21:36:22 +0700

    update file

--- tugasphpno2.php
+++ tugasphpno2.php
@@ -1,7 +1,7 @@
 <?php
-for ($i = 9; $i >= 1; $i--) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo "*";
+for ($i = 9; $i >= 1; $i--) { // membuat baris kebawah
+    for ($j = 1; $j <= $i; $j++) { //membuat data kesamping
+        echo "*"; // menampilkan output menjadi *
     }
-    echo "<br>";
+    echo "<br>"; // membuat enter
 }
--- tugasphpno3.php
+++ tugasphpno3.php
@@ -1,7 +1,7 @@
 <?php
-for ($i = 1; $i <= 9; $i++) {
-    for ($j = 0; $j < $i; $j++) {
-        echo "$j";
+for ($i = 1; $i <= 9; $i++) { //membuat baris kebawah
+    for ($j = 0; $j < $i; $j++) { //membuat data kesamping
+        echo "$j"; //menampilkan output dari $j
     }
-    echo "<br>";
+    echo "<br>"; //membuat enter
 }
